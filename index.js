a5a5ca56424d63ce423bbdbc548697e9bfcbf544
Jeric Chua <worldhail41@gmail.com>
2024-02-26 22:19:00 +0800

    rename public to docs folder to where html files are converted

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", "./views");
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get("/index", (req, res) => {
   return res.render("./pages/index");
